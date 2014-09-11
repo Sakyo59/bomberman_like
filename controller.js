03af9b21e0ba21b1d050b75cd52a86d08d46096a
RkTrail <RkTrail@github.com>
2014-09-11 17:15:38 +0200

    controller
    add controller.js
    
    controller is an object who we set keyboard input for control the player
    in game.

---
+++ controller.js
@@ -0,0 +1,63 @@
+function Controller(toucheHaut, toucheBas, toucheDroite, toucheGauche, toucheAction){
+	this.haut = false;
+	this.bas = false;
+	this.droite = false;
+	this.gauche = false;
+	this.bombe = false;
+
+	this.inputHaut = toucheHaut;
+	this.inputBas = toucheBas;
+	this.inputGauche = toucheGauche;
+	this.inputDroite = toucheDroite;
+	this.inputBombe = toucheAction;
+
+	this.event = {};
+	this.event.keyDown = function(e){
+		if(e.keyCode == this.inputHaut){
+			this.haut = true;
+			this.bas = false;
+			this.droite = false;
+			this.gauche = false;
+		}
+		else if(e.keyCode == this.inputDroite){
+			this.haut = false;
+			this.bas = false;
+			this.droite = true;
+			this.gauche = false;
+		}
+		else if(e.keyCode == this.inputGauche){
+			this.haut = false;
+			this.bas = false;
+			this.droite = false;
+			this.gauche = true;
+		}
+		else if(e.keyCode == this.inputBas){
+			this.haut = false;
+			this.bas = true;
+			this.droite = false;
+			this.gauche = false;
+		}
+
+		if(e.keyCode == this.inputBombe){
+			this.bombe = true;
+		}
+	}
+
+	this.event.keyUp = function(e){
+		if(e.KeyCode == this.inputHaut){
+			this.haut = false;
+		}
+		if(e.KeyCode == this.inputBas){
+			this.bas = false;
+		}
+		if(e.KeyCode == this.inputGauche){
+			this.gauche = false;
+		}
+		if(e.KeyCode == this.inputDroite){
+			this.droite = false;
+		}
+		if(e.KeyCode == this.inputBombe){
+			this.bombe = false;
+		}
+	}
+}
