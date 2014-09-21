650ffa349cb537c3304cf13f7a425111f421f9b2
Sakyo59 <renato.duriez@gmail.com>
2014-09-21 20:06:07 +0200

    modif html/css
    html5 et design

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+var context;
+var terrain;
+var joueur;
+var controle;
+
 function init (zoneDeJeu) {
-	var context=zoneDeJeu.getContext('2d');
+	context=zoneDeJeu.getContext('2d');
 	context.canvas.width=300;
 	context.canvas.height=300;
-	var terrain=new Terrain();
+	terrain=new Terrain();
 	terrain.loadMap(mapPlan);
-	var joueur=new Player(20,20,"P1");
-	var controle=new Controller(38,40,39,37,32);
+	joueur=new Player(20,20,"P1");
+	controle=new Controller(38,40,39,37,32);
 
 	document.addEventListener('keyDown', function(e){
 		controle.event.keyDown(e);
